ca135878e4d91bf702c3e9f6cc1fd8e4396816ea
bjurn84 <115032837+bjurn84@users.noreply.github.com>
2022-11-27 08:37:17 +0400

    Work36

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿Console.Write($"Введите количество элементов массива: ");
 int numberElements = Convert.ToInt32(Console.ReadLine()); 
-
 int RandomNumbers(int numberElements, int min, int max)
   {
   int[] randomNumbers = new int[numberElements];
@@ -21,5 +20,4 @@ int RandomNumbers(int numberElements, int min, int max)
   }
 
 int randomNumbers =  RandomNumbers(numberElements, 1, 10);
-
 Console.WriteLine($"\nСумма элементов, стоящих на нечётных позициях: {randomNumbers}");
